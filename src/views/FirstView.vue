98c6228eba16f6ba9efc5b9c1ed0d3a424e246d4
printlnCout <1304240844@qq.com>
2025-03-12 12:22:29 +0800

    测试提交

--- src/views/FirstView.vue
+++ src/views/FirstView.vue
@@ -3,11 +3,11 @@ import { inject } from 'vue';
 import { useMouse } from '../components/MouseMove';
 
 const { x, y } = useMouse();
-const i18n  = inject('i18n')
+const i18n  = inject('i18n');
 </script>
 
 <template>
-  First Mouse position is: {{ x }}, {{ y }}
+  The First Mouse position is: {{ x }}, {{ y }}
   <div>{{ $translate('greetings.hello') }}</div>
   <div>{{ i18n.greetings.hello }}</div>
 </template>
